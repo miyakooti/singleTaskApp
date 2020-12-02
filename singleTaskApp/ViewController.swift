f287455df7eb9f86815b3b9dc74c8e0d23de7fd6
arai kousuke <psychic.spoon313@gmail.com>
2020-12-02 17:53:46 +0900

    Initial Commit

---
+++ singleTaskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  singleTaskApp
+//
+//  Created by Arai Kousuke on 2020/12/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
